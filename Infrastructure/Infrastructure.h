e6c50cbb25d563060a3d40d9e234cbbe2fc0e92d
HuanLiu <iosboog@163.com>
2019-06-03 15:37:59 +0800

    first commit

---
+++ Infrastructure/Infrastructure.h
@@ -0,0 +1,19 @@
+//
+//  Infrastructure.h
+//  Infrastructure
+//
+//  Created by LiuHuan on 2019/6/3.
+//  Copyright © 2019 LH'sMacbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Infrastructure.
+FOUNDATION_EXPORT double InfrastructureVersionNumber;
+
+//! Project version string for Infrastructure.
+FOUNDATION_EXPORT const unsigned char InfrastructureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Infrastructure/PublicHeader.h>
+
+
---
+++ Infrastructure/UIDevice+Extension.h
@@ -0,0 +1,19 @@
+//
+//  UIDevice+Extension.h
+//  OPay
+//
+//  Created by SK_Wang on 2019/4/23.
+//  Copyright © 2019 Opera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIDevice (Extension)
+
+- (NSString *)deviceID;
+
+@end
+
+NS_ASSUME_NONNULL_END
